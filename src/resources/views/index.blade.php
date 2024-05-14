087146aeac31815367f7266d94394055cd6d78fa
takatsutsu <vivehodie3163@gmail.com>
2024-05-15 01:53:47 +0900

    バグ修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -59,47 +59,46 @@
             </div>
         </form>
     </div>
-    <form class="form_work_start" action="/workstart" method="post">
-        <div class="break_btn">
-            <form class="form_break_start" action="/breakstart" method="post">
-                @csrf
-                <div class="break_start">
-                    <input type="hidden" name="user_id" value="{{ $user->id }}" />
-                    <input type="hidden" name="user_name" value="{{ $user->name }}" />
-                    <input type="hidden" name="user_email" value="{{ $user->email }}" />
-                    <input type="hidden" name="break_date" />
-                    <input type="hidden" name="break_start_time" />
-                    <?php
-                    if ($btn1 == "B" || $btn1 == 'C') {
-                        $visu3 = "";
-                    }
-                    if ($btn1 == "A" || $btn1 == 'D' || $btn1 == 'E') {
-                        $visu3 = "disabled";
-                    }
-                    ?>
-                    <button class="break_start_btn" type=" submit" {{ $visu3 }}>休憩開始</button>
-                </div>
-            </form>
+    <div class="break_btn">
+        <form class="form_break_start" action="/breakstart" method="post">
+            @csrf
+            <div class="break_start">
+                <input type="hidden" name="user_id" value="{{ $user->id }}" />
+                <input type="hidden" name="user_name" value="{{ $user->name }}" />
+                <input type="hidden" name="user_email" value="{{ $user->email }}" />
+                <input type="hidden" name="break_date" />
+                <input type="hidden" name="break_start_time" />
+                <?php
+                if ($btn1 == "B" || $btn1 == 'C') {
+                    $visu3 = "";
+                }
+                if ($btn1 == "A" || $btn1 == 'D' || $btn1 == 'E') {
+                    $visu3 = "disabled";
+                }
+                ?>
+                <button class="break_start_btn" type=" submit" {{ $visu3 }}>休憩開始</button>
+            </div>
+        </form>
 
-            <form class="form_break_end" action="/breakend" method="post">
-                @csrf
-                <div class="break_end">
-                    <input type="hidden" name="user_id" value="{{ $user->id }}" />
-                    <input type="hidden" name="user_name" value="{{ $user->name }}" />
-                    <input type="hidden" name="user_email" value="{{ $user->email }}" />
-                    <input type="hidden" name="break_date" />
-                    <input type="hidden" name="break_start_time" />
-                    <?php
-                    if ($btn1 == "D") {
-                        $visu4 = "";
-                    }
-                    if ($btn1 == "A" || $btn1 == 'B' || $btn1 == 'C'   || $btn1 == 'E') {
-                        $visu4 = "disabled";
-                    }
-                    ?>
-                    <button class="break_end_btn" type="submit" {{ $visu4 }}>休憩終了</button>
-                </div>
-            </form>
-        </div>
+        <form class="form_break_end" action="/breakend" method="post">
+            @csrf
+            <div class="break_end">
+                <input type="hidden" name="user_id" value="{{ $user->id }}" />
+                <input type="hidden" name="user_name" value="{{ $user->name }}" />
+                <input type="hidden" name="user_email" value="{{ $user->email }}" />
+                <input type="hidden" name="break_date" />
+                <input type="hidden" name="break_start_time" />
+                <?php
+                if ($btn1 == "D") {
+                    $visu4 = "";
+                }
+                if ($btn1 == "A" || $btn1 == 'B' || $btn1 == 'C'   || $btn1 == 'E') {
+                    $visu4 = "disabled";
+                }
+                ?>
+                <button class="break_end_btn" type="submit" {{ $visu4 }}>休憩終了</button>
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
