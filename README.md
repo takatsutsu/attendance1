545561050b101997a6ab1d449d6b2cb0fcfdc306
takatsutsu <vivehodie3163@gmail.com>
2024-05-16 02:47:52 +0900

    readme シーディング修正

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# attendance1
-  勤怠管理システム
+## アプリケーション名
+
+勤怠管理システム
+![alt text](<スクリーンショット 2024-05-12 21.53.25.png>)
+
+## 作成した目的
+
+各社員が勤務開始、休憩開始、休憩終了、勤務終了ボタンを押下。  
+社員毎の勤務時間を管理できるようにする
+
+## アプリケーション URL
+
+- 打刻画面 http://localhost/
+- 日付別集計画面 http://localhost/sumsearch
+- 会員登録画面 http://localhost/register
+
+## 他のレポジトリー
+
+## 機能一覧
+
+- 会員登録
+- ログイン
+- ログアウト
+- 勤務開始
+- 勤務終了
+- 休憩開始
+- 休憩終了
+- 日付別集計画面
+- ページネーション
+
+## 使用技術(実行環境)
+
+- PHP 7.4.9
+- Laravel Framework 8.83.8
+- MySQL 8.0.26
+- phpmyadmin 5.2.1
+
+## テーブル設計
+
+![alt text](users.png)
+
+![alt text](attendees.png)
+
+![alt text](breaktimes.png)
+
+## ER 図
+
+![alt text](ER図-attendance1.png)
+
+## 環境構築
+
+**Docker ビルド**
+
+$ `cd coachteck/laravel`
+
+$ `git clone git@github.com:coachtech-material/`
+
+$ `laravel-docker-template.git`
+
+$ `mv laravel-docker-template`
+
+[attendannce1] [attendannce1]は任意のフォルダ名
+DockerDesktop アプリを立ち上げる
+
+$ `docker-compose up -d --build`
+
+開発の履歴を残すために、個人個人のリモートリポジトリの url を変更します。
+[attendannce1]名のリポジトリ―を GITHUB にて作成
+
+ターミナルより以下コマンドで　ローカルリポジトリのデータをリモートリポジトリに反映  
+$ `git add .`
+
+$ `git commit -m "リモートリポジトリの変更"`
+
+$ `git push origin main`
+
+**LARAVEL 環境構築**
+
+①$ `docker-compose exec php bash`
+
+②$ `composer install`
+
+③ 「.env.example」ファイルを 「.env」ファイルに命名を変更  
+ または、新しく.env ファイルを作成  
+④ .env に以下の環境変数を追加
+
+```
+DB_CONNECTION=mysql
+DB_HOST=mysql
+DB_PORT=3306
+DB_DATABASE=laravel_db
+DB_USERNAME=laravel_user
+DB_PASSWORD=laravel_pass
+```
+
+⑤ アプリケーションキーの作成  
+$ `docker-compose exec php bash`
+
+$ `php artisan key:generate`
+
+⑥ マイグレーションの実行  
+$ `docker-compose exec php bash`
+
+$ `php artisan migrate`
+
+⑦シーディングの実行(ユーザマスタ)  
+$ `docker-compose exec php bash`  
+
+$ `php artisan db:seed`  
+
+ログイン初期パスワードは全てpassword
+
+## 他に記載することがあれば記載する
---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => '山田太郎',
+            'email' => 'aa@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '佐藤次郎',
+            'email' => 'bb@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '鈴木三郎',
+            'email' => 'cc@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '加藤花子',
+            'email' => 'dd@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '井上陽子',
+            'email' => 'ee@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+
+        $param = [
+            'name' => '山下ひろみ',
+            'email' => 'ff@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '佐々木健一',
+            'email' => 'gg@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '加藤良子',
+            'email' => 'hh@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '吉田恵',
+            'email' => 'ii@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '本田亮一',
+            'email' => 'jj@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '斎藤まい',
+            'email' => 'kk@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '竹下恵子',
+            'email' => 'll@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '阿部貴子',
+            'email' => 'mm@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '森田康夫',
+            'email' => 'nn@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '松田栄子',
+            'email' => 'oo@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '中野啓介',
+            'email' => 'pp@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '橋本まりこ',
+            'email' => 'qq@gmail.com',
+            'password' => 'password'
+        ];
+        DB::table('users')->insert($param);
+    }
+}
